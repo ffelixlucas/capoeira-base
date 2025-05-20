3eea8a0a0ddcd29617446d82b740556bfcf11a62
Lucas Felix <lucas.fafx@gmail.com>
2025-05-20 16:11:37 -0300

    feat: proteger rota de upload na galeria com middleware verifyToken

--- backend/routes/galeriaRoutes.js
+++ backend/routes/galeriaRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const multer = require('multer');
 const { uploadImagem, listarImagens } = require('../controllers/galeriaController');
+const verifyToken = require('../middlewares/verifyToken')
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post('/upload', upload.single('imagem'), uploadImagem);
+router.post('/upload', verifyToken, upload.single('imagem'), uploadImagem);
 router.get('/', listarImagens);
 
 module.exports = router;
