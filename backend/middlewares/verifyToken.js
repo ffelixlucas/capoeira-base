52cffe37b0e2d69ecb80eaf9749bbfa61168795f
Lucas Felix <lucas.fafx@gmail.com>
2025-05-20 16:06:36 -0300

    feat: criar middleware verifyToken para autenticação JWT

---
+++ backend/middlewares/verifyToken.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+function verifyToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token não fornecido' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, usuario) => {
+    if (err) return res.status(403).json({ message: 'Token inválido' });
+    req.usuario = usuario;
+    next();
+  });
+}
+
+module.exports = verifyToken;
