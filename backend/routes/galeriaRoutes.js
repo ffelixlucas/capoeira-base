07cc59f4ac829199172e688870130b85eb46263d
Lucas Felix <lucas.fafx@gmail.com>
2025-05-21 15:42:31 -0300

    feat: registrar rota PUT /galeria/ordem protegida por JWT

--- backend/routes/galeriaRoutes.js
+++ backend/routes/galeriaRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const multer = require('multer');
-const { uploadImagem, listarImagens } = require('../controllers/galeriaController');
-const verifyToken = require('../middlewares/verifyToken')
+const {
+  uploadImagem,
+  listarImagens,
+  atualizarOrdem
+} = require('../controllers/galeriaController');
+const verifyToken = require('../middlewares/verifyToken');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post('/upload', verifyToken, upload.single('imagem'), uploadImagem);
 router.get('/', listarImagens);
+router.put('/ordem', verifyToken, atualizarOrdem); 
 
 module.exports = router;
