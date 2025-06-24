accd8682f0c7c1d57faf105c981aecbcb0d2d0dc
Lucas Felix <lucas.fafx@gmail.com>
2025-06-24 10:37:26 -0300

    refactor(core): conclui migração para arquitetura modular, remove módulos antigos e implementa refatoração de equipe, auth, horarios e agenda

--- backend/app.js
+++ backend/app.js
@@ -8,11 +8,11 @@ app.use(cors());
 app.use(express.json());
 
 // Importar rotas
-const galeriaRoutes = require('./routes/galeriaRoutes');
-const authRoutes = require('./routes/auth.Routes');
-const agendaRoutes = require('./routes/agendaRoutes');
-const horariosRoutes = require('./routes/horariosRoutes'); 
-const configuracoesRoutes = require('./routes/configuracoesRoutes');
+const galeriaRoutes = require('./modules/galeria/galeriaRoutes');
+const authRoutes = require('./modules/auth/auth.Routes');
+const agendaRoutes = require('./modules/agenda/agendaRoutes');
+const horariosRoutes = require('./modules/horarios/horariosRoutes'); 
+//const configuracoesRoutes = require('./modules/configuracoesRoutes');
 
 // Configurações do servidor
 
@@ -20,5 +20,5 @@ app.use('/api/auth', authRoutes);
 app.use('/api/galeria', galeriaRoutes);
 app.use('/api/agenda', agendaRoutes);
 app.use('/api/horarios', horariosRoutes); 
-app.use('/api/configuracoes', configuracoesRoutes);
+//app.use('/api/configuracoes', configuracoesRoutes);
 module.exports = app;
