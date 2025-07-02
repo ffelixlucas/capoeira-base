e3061a980471df07f1eaa968bd4246ebde0876b6
Lucas Felix <lucas.fafx@gmail.com>
2025-07-02 16:33:42 -0300

    fix: atualiza connection.js para usar variáveis de ambiente

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'senha123', // ou a senha que você configurou
-  database: 'capoeira_base',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
